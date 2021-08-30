5569d5ea92f0e387cd872e5485ab1a2e3b627970
mikecole <cole.mike@gmail.com>
2021-08-30 18:08:33 +0000

    AU: 1 updated - ssdt17 [skip ci] https://gist.github.com/fc383d2a7d15fa7825ca98990d04357a/d5aa49371283ab457782da595c68d8d32ecf9df6

--- automatic/ssdt17/tools/chocolateyInstall.ps1
+++ automatic/ssdt17/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = 'f8556f4d4f3e876e0df26c1d3249430043b12b7c48f520a3facbc83f0cefdd27'
-$url = 'https://go.microsoft.com/fwlink/?linkid=2161172'
+﻿$checksum = '813ef6867d3c8c8c8d27a8deec5128b5e2d0c2d6fd4efb80e3435da718b96453'
+$url = 'https://go.microsoft.com/fwlink/?linkid=2169967'
 
 (Get-WmiObject -Class Win32_OperatingSystem).Version -match "(?<Major>\d+).(?<Minor>\d+).(?<Build>\d+)" | Out-Null
  
