580a162b44771c762f402deff9661848f088e45f
mikecole <cole.mike@gmail.com>
2022-04-12 18:07:38 +0000

    AU: 1 updated - microsoftazurestorageexplorer [skip ci] https://gist.github.com/fc383d2a7d15fa7825ca98990d04357a/7397ba853c8e5860f510f10009b3118c9f00c1e6

--- automatic/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
+++ automatic/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $checksum = '5b1b672656daf485e0920805cfc797a337487c0c17782d3fd3a6f102c4eeada9'
-$url = 'https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.23.0/Windows_StorageExplorer.exe'
+$url = 'https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.23.1/Windows_StorageExplorer.exe'
 
 $packageArgs = @{
   packageName    = 'microsoftazurestorageexplorer'
