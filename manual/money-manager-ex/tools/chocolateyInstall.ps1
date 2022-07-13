12afa3784cac52c3353b596c1f3bc4ad708b0c25
Mike Cole <cole.mike@gmail.com>
2022-07-13 12:07:07 -0500

    Updated money-manager-ex

--- manual/money-manager-ex/tools/chocolateyInstall.ps1
+++ manual/money-manager-ex/tools/chocolateyInstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   silentArgs     = '/VERYSILENT /NORESTART'
   validExitCodes = @(0)
   softwareName   = 'MoneyManagerEX*'
-  url32bit       = 'https://github.com/moneymanagerex/moneymanagerex/releases/download/v1.3.6/mmex-1.3.6-win32.exe'
-  checksum32     = 'E7B5F133DE6A2D1FDC4150D41954980FF27DA817E68F7358EF37BC73DA2AC8C3'
+  url32bit       = 'https://github.com/moneymanagerex/moneymanagerex/releases/download/v1.5.16/mmex-1.5.16-win32.exe'
+  checksum32     = '773dfd75933224a4af5fbf9fd915f7acabfab9988fa11146a6690a7fe27badb2'
   checksumType32 = 'sha256'
-  url64bit       = 'https://github.com/moneymanagerex/moneymanagerex/releases/download/v1.3.6/mmex-1.3.6-win64.exe'
-  checksum64     = '25118A25F3CC7982CE8326F3FEFF73A909BE50C8AC85CDF1BE04756E820121E6'
+  url64bit       = 'https://github.com/moneymanagerex/moneymanagerex/releases/download/v1.5.16/mmex-1.5.16-win64.exe'
+  checksum64     = '795d9652abf1d396f703622185b9771fb317e00c4ff67185a4853752c68fa24f'
   checksumType64 = 'sha256'
 }
 
