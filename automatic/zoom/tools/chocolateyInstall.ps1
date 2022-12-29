8ce6b989b9fe8871a65d34a54479465786835498
mikecole <cole.mike@gmail.com>
2022-12-29 14:06:38 +0000

    AU: 1 updated - zoom [skip ci] https://gist.github.com/fc383d2a7d15fa7825ca98990d04357a/a6189b788be7d4abd394d5aa5943348c2f8f0265

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '5154f2ab061b3d2f1bbe1dd14d755adc619cc6be85ca9bfc17b9686f13f83b04'
-$checksum64 = '4e3d46e6014e7e50abc5c89f67e3846d1cb132ed039912e462288b2cd305223d'
+$checksum = '36b7bc22c4bab7063f230eb4cb4390f0c691de840632e3991cd94ae070f529e0'
+$checksum64 = 'b6e29e1672b18699fd7d38e21160fdf55f1ebfa6c04f36c5d6e35493fdfca6eb'
 
-$url = 'https://cdn.zoom.us/prod/5.13.0.11216/ZoomInstallerFull.msi'
-$url64 = 'https://cdn.zoom.us/prod/5.13.0.11216/x64/ZoomInstallerFull.msi'
+$url = 'https://cdn.zoom.us/prod/5.13.3.11494/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/5.13.3.11494/x64/ZoomInstallerFull.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
