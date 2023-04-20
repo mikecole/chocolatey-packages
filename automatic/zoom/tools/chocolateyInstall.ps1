415ab121e41ea50689a269257d853438ff8ec99a
Orion Poplawski <orion@nwra.com>
2023-04-20 15:28:11 -0600

    [zoom] Add package parameters

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -5,14 +5,31 @@ $checksum64 = '3c5765f01836a9491b504c259bc6751a1704f545d0e644cb29c36af7bb9ba2cd'
 $url = 'https://cdn.zoom.us/prod/5.14.5.15287/ZoomInstallerFull.msi'
 $url64 = 'https://cdn.zoom.us/prod/5.14.5.15287/x64/ZoomInstallerFull.msi'
 
+$silentArgs = '/quiet /qn /norestart'
+
+$pp = Get-PackageParameters
+
+if ($pp['DisableRestartManager']) { $silentArgs += " MSIRestartManagerControl=Disable" }
+if ($pp['NoAutoUpdate']) { $silentArgs += " ZoomAutoUpdate=False" }
+else { $silentArgs += " ZoomAutoUpdate=True" }
+if ($pp['NoDesktopShortcut']) { $silentArgs += " zNoDesktopShortCut=True" }
+if ($pp['NoInstallIfRunning']) { 
+  if (Get-Process zoom -ea 0) {
+    Write-Warning "Exiting installation because Zoom is running and /NoInstallIfRunning was passed."
+    exit 1
+  }
+}
+if ($pp['SilentStart']) { $silentArgs += " zSilentStart=True" }
+if ($pp['SSOHost']) { $silentArgs += " zSSOHost=$(pp['SSOHost'])" }
+
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
   url            = $url
   url64          = $url64
-  silentArgs     = '/quiet /norestart ZoomAutoUpdate=True'
-  validExitCodes = @(0)
+  silentArgs     = $silentArgs
+  validExitCodes = @(0, 3010)
   softwareName   = 'zoom*'
   checksum       = $checksum
   checksum64     = $checksum64
