18be225dc582b0568e405a6e598b6ac1c4b0e0cd
mikecole <cole.mike@gmail.com>
2023-06-27 13:06:45 +0000

    AU: 1 updated - zoom [skip ci]

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '766e981bd2e810e18a883d669799d4a38a86eb58adfe32d51f99be7cc17e98c5'
-$checksum64 = 'f3f85d3c3dd6dc342efa3e91ec7649c77e8a770d8b009ea7830c8d360e850497'
+$checksum = '0a649a20e91f4278753586b940ae7a23f6f62bfe8ac43dff4daf2acfb1da9792'
+$checksum64 = 'bd2740f8fcbe751babf6127d617753e548fd51e9b0bd514e09260610f239a879'
 
-$url = 'https://cdn.zoom.us/prod/5.15.1.17948/ZoomInstallerFull.msi'
-$url64 = 'https://cdn.zoom.us/prod/5.15.1.17948/x64/ZoomInstallerFull.msi'
+$url = 'https://cdn.zoom.us/prod/5.15.2.18096/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/5.15.2.18096/x64/ZoomInstallerFull.msi'
 
 $silentArgs = '/quiet /qn /norestart'
 
