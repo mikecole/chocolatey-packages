8ac3e395fbe34a1fe7b76cc1d9ae318a901dbfb3
Mike Cole <cole.mike@gmail.com>
2024-05-13 18:28:55 -0700

    Updated ringcentral-classic

--- manual/ringcentral-classic/tools/chocolateyInstall.ps1
+++ manual/ringcentral-classic/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   validExitCodes = @(0)
   softwareName   = 'RingCentral Classic*'
   url64bit       = 'https://app.ringcentral.com/download/RingCentral.exe?V=2012528645154747305'
-  checksum64     = '58253fbb65c44eedebc6c562e83f967d424bf17f90fe4586add754b7dd4b408e'
+  checksum64     = '4b00b6b3845daa146315100a0645340862d8537e756ca701364086fc675474b6'
   checksumType64 = 'sha256'
 }
 
