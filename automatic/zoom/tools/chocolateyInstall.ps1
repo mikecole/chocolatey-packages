b263eac3f39456b9bd3a4424aa9800f356c618af
Mike Cole <cole.mike@gmail.com>
2024-06-03 22:57:29 -0500

    Manually ran update_all

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = 'e5448484226751394fa356ec7036703d9f97416edb9367337e4c5f2046be4f3f'
-$checksum64 = '7efa2706ad420733843684d1f6f7e1df23c6af0e802b11ef88a5c3be189cea50'
+$checksum = 'f73d5da8f8375236bcb88df999d17f9c78cd308275fc3f5614541a8aff83e638'
+$checksum64 = '045c4850f6395b76555ed1df4353ca03a09898b56ec6ce8c4fa01f58179a0d63'
 
-$url = 'https://cdn.zoom.us/prod/6.0.10.39647/ZoomInstallerFull.msi'
-$url64 = 'https://cdn.zoom.us/prod/6.0.10.39647/x64/ZoomInstallerFull.msi'
+$url = 'https://cdn.zoom.us/prod/6.0.11.39959/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/6.0.11.39959/x64/ZoomInstallerFull.msi'
 
 $silentArgs = '/quiet /qn /norestart'
 
