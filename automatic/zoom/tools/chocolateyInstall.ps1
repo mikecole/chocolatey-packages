197533841e4d1f6a6eb039cba35b50c639425305
mikecole <cole.mike@gmail.com>
2025-03-18 09:07:34 +0000

    AU: 1 updated - zoom [skip ci] https://gist.github.com/mikecole/fc383d2a7d15fa7825ca98990d04357a/7255e2785e5440f0f302ea60f2506838a481e7aa

--- automatic/zoom/tools/chocolateyInstall.ps1
+++ automatic/zoom/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '4e7137fb4b349325b2880fff98a473cc1179bd15bc244e73f3b6861a53ffff86'
-$checksum64 = '9197f8bfb0ad6737ffa3b74c487c0dfab3ae08c51cd64e9b4395b3471bc5940d'
+$checksum = '08c1bb025bc782fd88268a4a9f817a1f2f64c93dcbf29b1ebbcaa8f7b710b158'
+$checksum64 = '04ad1a7fbe543517acad73f61be3f1e75560351f55f0e8d18069931b04c16df0'
 
-$url = 'https://cdn.zoom.us/prod/6.3.11.60501/ZoomInstallerFull.msi'
-$url64 = 'https://cdn.zoom.us/prod/6.3.11.60501/x64/ZoomInstallerFull.msi'
+$url = 'https://cdn.zoom.us/prod/6.4.0.62047/ZoomInstallerFull.msi'
+$url64 = 'https://cdn.zoom.us/prod/6.4.0.62047/x64/ZoomInstallerFull.msi'
 
 $silentArgs = '/quiet /qn /norestart'
 
